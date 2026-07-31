9ac103f3f7bba5f282d7574fc23dba9a302aed4c
as662012-max <as662012@gmail.com>
2026-07-31 14:50:57 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>It works 🎉</h1>
-      <p>This is your Next.js app running on Vercel.</p>
-    </div>
-  );
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
