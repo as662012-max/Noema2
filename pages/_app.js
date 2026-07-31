93e913b559bda7e5fddbd6c9760f0afd0460a6fc
as662012-max <as662012@gmail.com>
2026-07-31 14:37:24 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>It works 🎉</h1>
+      <p>This is your Next.js app running on Vercel.</p>
+    </div>
+  );
+}
